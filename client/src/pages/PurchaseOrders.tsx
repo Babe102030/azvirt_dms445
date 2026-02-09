f003f8b18114eaf46ceb15239ebf134b662a58ec
niksisdamir-rgb <niksisdamir@gmail.com>
2026-02-09 11:04:44 +0100

    feat: Add tabs to Purchase Orders page

--- client/src/pages/PurchaseOrders.tsx
+++ client/src/pages/PurchaseOrders.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Dialog,
   DialogContent,
