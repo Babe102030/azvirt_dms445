7c7557526505b441b3a06e6610c63c01703173d8
niksisdamir-rgb <niksisdamir@gmail.com>
2026-02-11 04:46:28 +0100

    Remove generated DB queries, AI docs and tests

--- test_import.ts
+++
@@ -1,3 +0,0 @@
-import * as schema from './drizzle/schema';
-console.log('Schema loaded successfully');
-console.log('Tables:', Object.keys(schema).filter(k => typeof schema[k] === 'object' && schema[k] && 'name' in schema[k]));
